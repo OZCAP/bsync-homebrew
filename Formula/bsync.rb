660eb41637da9d2bdee8ab6fc8adee9c23adaafb
Oscar <48883102+OZCAP@users.noreply.github.com>
2023-02-08 17:49:52 +0000

    Update bsync.rb

--- Formula/bsync.rb
+++ Formula/bsync.rb
@@ -4,6 +4,8 @@ class Bsync < Formula
     url "https://github.com/OZCAP/bsync/releases/download/0.1.0/bsync-mac.tar.gz"
     sha256 "d7efd512eb486185f117ed05cdf1e58490723208972e92fb5c2a1cee1d3d9081"
     version "0.1.0"
+    
+    depends_on: "gh"
   
     def install
       bin.install "bsync"
