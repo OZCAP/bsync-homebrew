373f1325ae0a4c4ca6468fe2ac4f6a84f71e3997
Oscar <48883102+OZCAP@users.noreply.github.com>
2023-06-29 13:14:45 +0300

    Release 0.2.0

--- Formula/bsync.rb
+++ Formula/bsync.rb
@@ -1,9 +1,9 @@
 class Bsync < Formula
     desc "Git branch management and project synchronization tool"
     homepage "https://github.com/OZCAP/bsync"
-    url "https://github.com/OZCAP/bsync/releases/download/0.1.0/bsync-mac.tar.gz"
-    sha256 "d7efd512eb486185f117ed05cdf1e58490723208972e92fb5c2a1cee1d3d9081"
-    version "0.1.0"
+    url "https://github.com/OZCAP/bsync/releases/download/0.2.0/bsync-mac.tar.gz"
+    sha256 "a35e22793b9478334cb4164927ae2877f58011edfb9b22c7b24f2966f5ba15c8"
+    version "0.2.0"
   
     def install
       bin.install "bsync"
